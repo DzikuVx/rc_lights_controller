92fcde7c0f196b546d562149f42ba87560903129
Pawel <pspychalski@gmail.com>
2016-01-26 21:37:22 +0100

    button driven mode when no RC input is available

--- rc_lights_controller.ino
+++ rc_lights_controller.ino
@@ -1,9 +1,13 @@
+#include <EEPROM.h>
 #include <PinChangeInterrupt.h>
 
 #define OUTPUT_CHANNELS 3
 #define LPF_FACTOR 0.8
 #define MAX_TICK 127
 #define CHANGES_PER_CHANNEL 8
+#define CYCLE_OFF 255
+#define CYCLE_ON 0
+#define BUTTON_PIN 15
 
 /*
  * Define pins used to provide RC PWM signal to Arduino
@@ -17,14 +21,11 @@ volatile unsigned int channel_length[OUTPUT_CHANNELS] = {0, 0};
 
 const byte output_pin[OUTPUT_CHANNELS] = {10, 16, 14};
 
-#define CYCLE_OFF 255
-#define CYCLE_ON -1
-
 /*
- * Use -1 to light it always
+ * Do not use 0 or values above MAX_TICK
  * 255 no light never
  */
-int patterns[9][OUTPUT_CHANNELS][CHANGES_PER_CHANNEL] = {
+const byte patterns[9][OUTPUT_CHANNELS][CHANGES_PER_CHANNEL] = {
   {{CYCLE_OFF}, {CYCLE_OFF}, {CYCLE_OFF}}, //CH1 LOW, CH2 LOW or not connected
   {{9, 10, 19, 20, 29, 30, 39, 40}, {9, 10, 19, 20, 29, 30, 39, 40}, {CYCLE_OFF}}, //CH1 MID, CH2 LOW or not connected
   {{CYCLE_ON}, {CYCLE_ON}, {CYCLE_ON}}, //CH1 HIGH, CH2 LOW or not connected
@@ -36,15 +37,16 @@ int patterns[9][OUTPUT_CHANNELS][CHANGES_PER_CHANNEL] = {
   {{CYCLE_OFF}, {CYCLE_OFF}, {CYCLE_ON}} //CH1 HIGH, CH2 HIGH
 };
 
-//{9, 10, 19, 20, 29, 30, 39, 40}, 10% PWM
-//{5, 10, 15, 20, 25, 30, 35, 40}, 50% PWM
-//{20,21,22,23,43,44,45,46}, Blink-blink
+const byte buttonModes[6] = {0, 1, 4, 7, 5, 8};
+byte buttonMode = 0;
+byte previousButtonState = HIGH;
+byte currentButtonState = LOW;
 
 void setup() {
-//  Serial.begin(57600);
 
   pinMode(channel_pin[0], INPUT);
   pinMode(channel_pin[1], INPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
 
   pinMode(output_pin[0], OUTPUT);
   pinMode(output_pin[1], OUTPUT);
@@ -52,12 +54,11 @@ void setup() {
 
   attachPinChangeInterrupt(digitalPinToPinChangeInterrupt(channel_pin[0]), onRising0, CHANGE);
   attachPinChangeInterrupt(digitalPinToPinChangeInterrupt(channel_pin[1]), onRising1, CHANGE);
-  
 }
 
 int smooth(int data, float filterVal, float smoothedVal){
 
-  if (filterVal > 1){      // check to make sure params are within range
+  if (filterVal > 1){ // check to make sure params are within range
     filterVal = .99;
   }
   else if (filterVal <= 0){
@@ -88,11 +89,11 @@ void onRising1(void) {
   processPin(1);
 }
 
-bool masterSwitch = false;
+bool rcModeEnabled = false;
 
 byte output_table[3] = {false, false, false};
 
-byte pattern = 2;
+byte pattern = 0;
 byte patternPrevious = 0;
 byte patternIndex[OUTPUT_CHANNELS] = {0, 0, 0};
 
@@ -124,41 +125,54 @@ byte channelLengthToPosition(unsigned int channelLength) {
 void loop() {
 
   if (channel_length[0] > 600 && channel_length[0] < 2400) {
-    masterSwitch = true;
+    rcModeEnabled = true;
   } else {
-    masterSwitch = false;
+    rcModeEnabled = false;
   }
 
-  if (masterSwitch) {
-
+  if (rcModeEnabled) {
     pattern = (3 * channelLengthToPosition(channel_length[1])) + channelLengthToPosition(channel_length[0]);
+  } else {
+    pattern = buttonModes[buttonMode];
+  }
+  if (pattern != patternPrevious) {
+    resetOutput();
+  }
 
-    if (pattern != patternPrevious) {
-      resetOutput();
+  for (byte i = 0; i < OUTPUT_CHANNELS; i++) {
+    currentPattern = patterns[pattern][i][patternIndex[i]];
+
+    if (currentPattern == CYCLE_ON) {
+      output_table[i] = true;
+    } else if (tick[i] == currentPattern) {
+      patternIndex[i]++;    
+      output_table[i] = !output_table[i];
     }
 
-    for (byte i = 0; i < OUTPUT_CHANNELS; i++) {
-      currentPattern = patterns[pattern][i][patternIndex[i]];
-  
-      if (currentPattern == CYCLE_ON) {
-        output_table[i] = true;
-      } else if (tick[i] == currentPattern) {
-        patternIndex[i]++;    
-        output_table[i] = !output_table[i];
-      }
-  
-      tick[i]++;
-  
-      if (patternIndex[i] == CHANGES_PER_CHANNEL) {
-        tick[i] = 0;
-        patternIndex[i] = 0;
-      }
-  
-      if (tick[i] == MAX_TICK) {
-        tick[i] = 0;
-        patternIndex[i] = 0;
+    tick[i]++;
+
+    if (patternIndex[i] == CHANGES_PER_CHANNEL) {
+      tick[i] = 0;
+      patternIndex[i] = 0;
+    }
+
+    if (tick[i] == MAX_TICK) {
+      tick[i] = 0;
+      patternIndex[i] = 0;
+    }
+  }
+
+  if (!rcModeEnabled) {
+
+    currentButtonState = digitalRead(BUTTON_PIN);
+
+    if (currentButtonState == LOW && previousButtonState == HIGH) {
+      buttonMode++;
+      if (buttonMode >= sizeof(buttonModes)) {
+        buttonMode = 0;
       }
     }
+    previousButtonState = currentButtonState;
   }
 
   patternPrevious = pattern;
